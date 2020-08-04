0c6be2b722e2b2d3f821898aa562866341dc9067
Juanchi36 <juanmartingon@hotmail.com>
2020-08-03 21:09:27 -0300

    Updates

---
+++ precache-manifest.1940edd51ab10a0b667b7c810244acdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b47fa00f62395220e23a01ba2edc45",
+    "url": "/fvg/index.html"
+  },
+  {
+    "revision": "d2be09af65fb158bb083",
+    "url": "/fvg/static/css/main.0098f2d2.chunk.css"
+  },
+  {
+    "revision": "82de33e7c1fc494ca00b",
+    "url": "/fvg/static/js/2.ca18af94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fvg/static/js/2.ca18af94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2be09af65fb158bb083",
+    "url": "/fvg/static/js/main.2a535f47.chunk.js"
+  },
+  {
+    "revision": "c498d676a2b4e2ba8c5f",
+    "url": "/fvg/static/js/runtime-main.ab12838b.js"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/fvg/static/media/pokeball.2b4798f5.svg"
+  }
+]);
